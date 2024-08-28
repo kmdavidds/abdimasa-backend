5343a3c2e98e0593f61613ed1e25ab88e7f6e7c5
kmdavidds <kmdavidds@gmail.com>
2024-08-29 02:45:22 +0700

    feat: add Remark entity

---
+++ internal/pkg/entity/remark.go
@@ -0,0 +1,15 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Remark struct {
+	ID         uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
+	Name       string    `gorm:"type:varchar(255);not null" json:"name"`
+	Occupation string    `gorm:"type:varchar(255);not null" json:"occupation"`
+	Gender     string    `gorm:"type:varchar(255);not null" json:"gender"`
+	CreatedAt  time.Time `gorm:"type:timestamptz;not null" json:"createdAt"`
+}
