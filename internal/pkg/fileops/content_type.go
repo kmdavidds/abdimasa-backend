0fdcc01cea6ee637b83ec9e44fa3c7e7697296e8
kmdavidds <kmdavidds@gmail.com>
2024-10-10 14:18:35 +0700

    feat: add fileops pkg

---
+++ internal/pkg/fileops/content_type.go
@@ -0,0 +1,45 @@
+package fileops
+
+import (
+	"log"
+	"mime/multipart"
+	"net/http"
+)
+
+var ImageContentTypes = []string{
+	"image/apng",
+	"image/avif",
+	"image/bmp",
+	"image/gif",
+	"image/vnd.microsoft.icon",
+	"image/jpeg",
+	"image/png",
+	"image/svg+xml",
+	"image/tiff",
+	"image/webp",
+}
+
+var DocumentContentTypes = []string{
+	"application/pdf",
+}
+
+func DetectMultipartFileType(file *multipart.FileHeader) (string, error) {
+	fileContent, err := file.Open()
+	if err != nil {
+		return "", err
+	}
+	defer func(fileContent multipart.File) {
+		err := fileContent.Close()
+		if err != nil {
+			log.Println("Error closing file: ", err)
+		}
+	}(fileContent)
+
+	buffer := make([]byte, 512)
+	_, err = fileContent.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+
+	return http.DetectContentType(buffer), nil
+}
