b2534d4a8b8882f58080119dbbd01c4581e998a1
kmdavidds <kmdavidds@gmail.com>
2024-10-10 14:20:43 +0700

    feat: add image upload to Activity

--- internal/app/service/activity.go
+++ internal/app/service/activity.go
@@ -1,11 +1,15 @@
 package service
 
 import (
+	"slices"
+
 	"github.com/google/uuid"
 	"github.com/kmdavidds/abdimasa-backend/internal/app/repository"
 	"github.com/kmdavidds/abdimasa-backend/internal/pkg/dto"
 	"github.com/kmdavidds/abdimasa-backend/internal/pkg/entity"
 	"github.com/kmdavidds/abdimasa-backend/internal/pkg/errors"
+	"github.com/kmdavidds/abdimasa-backend/internal/pkg/fileops"
+	"github.com/kmdavidds/abdimasa-backend/internal/pkg/supabase"
 	"github.com/kmdavidds/abdimasa-backend/internal/pkg/validator"
 )
 
@@ -19,13 +23,15 @@ type ActivityService interface {
 type activityService struct {
 	ar  repository.ActivityRepository
 	val validator.Validator
+	supabase supabase.Supabase
 }
 
 func NewActivityService(
 	ar repository.ActivityRepository,
 	val validator.Validator,
+	supabase supabase.Supabase,
 ) ActivityService {
-	return &activityService{ar, val}
+	return &activityService{ar, val, supabase}
 }
 
 func (as *activityService) Create(req dto.CreateActivityRequest) error {
@@ -33,6 +39,30 @@ func (as *activityService) Create(req dto.CreateActivityRequest) error {
 	if valErr != nil {
 		return valErr
 	}
+
+	var imageURL = ""
+
+	if req.Image1 != nil {
+		if req.Image1.Size > 1*fileops.MegaByte {
+			return errors.ErrorFileTooLarge
+		}
+
+		fileType, err := fileops.DetectMultipartFileType(req.Image1)
+
+		if err != nil {
+			return errors.ErrorInvalidFileType
+		}
+
+		allowedTypes := fileops.ImageContentTypes
+		if !slices.Contains(allowedTypes, fileType) {
+			return errors.ErrorInvalidFileType
+		}
+
+		imageURL, err = as.supabase.Upload(req.Image1)
+		if err != nil {
+			return err
+		}
+	}	
 
 	idV7, err := uuid.NewV7()
 	if err != nil {
@@ -42,7 +72,7 @@ func (as *activityService) Create(req dto.CreateActivityRequest) error {
 	activity := entity.Activity{
 		ID:       idV7,
 		Title:    req.Title,
-		ImageURL: req.ImageURL,
+		ImageURL: imageURL,
 		Date:     req.Date,
 		Time:     req.Time,
 		Location: req.Location,
@@ -72,10 +102,34 @@ func (as *activityService) Update(req dto.UpdateActivityRequest) error {
 		return valErr
 	}
 
+	var imageURL = ""
+
+	if req.Image1 != nil {
+		if req.Image1.Size > 1*fileops.MegaByte {
+			return errors.ErrorFileTooLarge
+		}
+
+		fileType, err := fileops.DetectMultipartFileType(req.Image1)
+
+		if err != nil {
+			return errors.ErrorInvalidFileType
+		}
+
+		allowedTypes := fileops.ImageContentTypes
+		if !slices.Contains(allowedTypes, fileType) {
+			return errors.ErrorInvalidFileType
+		}
+
+		imageURL, err = as.supabase.Upload(req.Image1)
+		if err != nil {
+			return err
+		}
+	}	
+
 	activity := entity.Activity{
 		ID:       req.ID,
 		Title:    req.Title,
-		ImageURL: req.ImageURL,
+		ImageURL: imageURL,
 		Date:     req.Date,
 		Time:     req.Time,
 		Location: req.Location,
