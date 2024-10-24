5580f4b54fdb25c41810f4b4b6495dabd355f179
kmdavidds <kmdavidds@gmail.com>
2024-10-24 07:21:24 +0700

    fix: hotfix news imageURL field to image1 in json

--- internal/pkg/entity/news.go
+++ internal/pkg/entity/news.go
@@ -10,6 +10,6 @@ type News struct {
 	ID          uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
 	Title       string    `gorm:"type:varchar(255);not null" json:"title"`
 	Description string    `gorm:"type:text;not null" json:"description"`
-	ImageURL    string    `gorm:"type:text" json:"imageURL"`
+	ImageURL    string    `gorm:"type:text" json:"image1"`
 	CreatedAt   time.Time `gorm:"type:timestamptz;not null" json:"createdAt"`
 }
